5f52c2708eaf6eaf5ae17591fc0faad571d7ddb0
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-11-29 20:52:16 +0300

    feat: add multiplier2, keccak, rsa prover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,263 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { CircuitSignals, Groth16Proof, PublicSignals } from "snarkjs";
+
+const url = "https://mopro.vivianjeng.xyz";
+
+async function getKeys(circuit: string) {
+    const wasmUrl = new URL(`${circuit}.wasm`, url).toString();
+    const zkeyUrl = new URL(`${circuit}_final.zkey`, url).toString();
+    const wasm = await fetch(wasmUrl).then((r) => r.arrayBuffer());
+    const zkey = await fetch(zkeyUrl).then((r) => r.arrayBuffer());
+    return { wasm, zkey };
+}
+
+async function fullProve(circuit: string, inputs: CircuitSignals) {
+    const _snarkjs = import("snarkjs");
+    const snarkjs = await _snarkjs;
+    const { wasm, zkey } = await getKeys(circuit);
+    const start = +Date.now();
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+        inputs,
+        new Uint8Array(wasm),
+        new Uint8Array(zkey)
+    );
+    const end = +Date.now();
+    return { proof, publicSignals, provingTime: end - start };
+}
+
+// async function verifyProof(
+//     circuit: string,
+//     proof: Groth16Proof,
+//     publicSignals: PublicSignals
+// ) {
+//     const _snarkjs = import("snarkjs");
+//     const vkeyUrl = new URL(`${circuit}.vkey.json`, url).toString();
+//     const vkeyBuffer = await fetch(vkeyUrl).then((r) => r.arrayBuffer());
+//     const vkeyString = String.fromCharCode.apply(
+//         null,
+//         new Uint8Array(vkeyBuffer) as any
+//     );
+//     const vkey = JSON.parse(vkeyString);
+//     const snarkjs = await _snarkjs;
+//     const start = +Date.now();
+//     const valid = await snarkjs.groth16.verify(vkey, publicSignals, proof);
+//     const end = +Date.now();
+//     return { valid: valid, verifyingTime: end - start };
+// }
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [multiplier2ProvingTime, setMultiplier2ProvingTime] =
+        useState<string>("");
+    const [multiplier2Proof, setMultiplier2Proof] = useState<string>();
+    const [multiplier2PublicSignals, setMultiplier2PublicSignals] =
+        useState<string>("");
+    const [keccak256ProvingTime, setKeccak256ProvingTime] =
+        useState<string>("");
+    const [keccak256Proof, setKeccak256Proof] = useState<string>("");
+    const [keccak256PublicSignals, setKeccak256PublicSignals] =
+        useState<string>("");
+    const [RSAProvingTime, setRSAProvingTime] = useState<string>("");
+    const [RSAProof, setRSAProof] = useState<string>();
+    const [RSAPublicSignals, setRSAPublicSignals] = useState<string>("");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    async function multiplier() {
+        setMultiplier2ProvingTime("Calculating...");
+        const { proof, publicSignals, provingTime } = await fullProve(
+            "multiplier2",
+            { a: 3, b: 5 }
+        );
+        setMultiplier2ProvingTime(`${provingTime / 1000} s`);
+        setMultiplier2Proof(JSON.stringify(proof));
+        setMultiplier2PublicSignals(JSON.stringify(publicSignals));
+    }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    async function keccak256() {
+        setKeccak256ProvingTime("Calculating...");
+        const { proof, publicSignals, provingTime } = await fullProve(
+            "keccak256_256_test",
+            {
+                in: [
+                    0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0,
+                    1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                ],
+            }
+        );
+        setKeccak256ProvingTime(`${provingTime / 1000} s`);
+        setKeccak256Proof(JSON.stringify(proof));
+        setKeccak256PublicSignals(JSON.stringify(publicSignals));
+    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    async function RSA() {
+        setRSAProvingTime("Calculating...");
+        const { proof, publicSignals, provingTime } = await fullProve("main", {
+            modulus: [
+                "13792647154200341559",
+                "12773492180790982043",
+                "13046321649363433702",
+                "10174370803876824128",
+                "7282572246071034406",
+                "1524365412687682781",
+                "4900829043004737418",
+                "6195884386932410966",
+                "13554217876979843574",
+                "17902692039595931737",
+                "12433028734895890975",
+                "15971442058448435996",
+                "4591894758077129763",
+                "11258250015882429548",
+                "16399550288873254981",
+                "8246389845141771315",
+                "14040203746442788850",
+                "7283856864330834987",
+                "12297563098718697441",
+                "13560928146585163504",
+                "7380926829734048483",
+                "14591299561622291080",
+                "8439722381984777599",
+                "17375431987296514829",
+                "16727607878674407272",
+                "3233954801381564296",
+                "17255435698225160983",
+                "15093748890170255670",
+                "15810389980847260072",
+                "11120056430439037392",
+                "5866130971823719482",
+                "13327552690270163501",
+            ],
+            signature: [
+                "3582320600048169363",
+                "7163546589759624213",
+                "18262551396327275695",
+                "4479772254206047016",
+                "1970274621151677644",
+                "6547632513799968987",
+                "921117808165172908",
+                "7155116889028933260",
+                "16769940396381196125",
+                "17141182191056257954",
+                "4376997046052607007",
+                "17471823348423771450",
+                "16282311012391954891",
+                "70286524413490741",
+                "1588836847166444745",
+                "15693430141227594668",
+                "13832254169115286697",
+                "15936550641925323613",
+                "323842208142565220",
+                "6558662646882345749",
+                "15268061661646212265",
+                "14962976685717212593",
+                "15773505053543368901",
+                "9586594741348111792",
+                "1455720481014374292",
+                "13945813312010515080",
+                "6352059456732816887",
+                "17556873002865047035",
+                "2412591065060484384",
+                "11512123092407778330",
+                "8499281165724578877",
+                "12768005853882726493",
+            ],
+            base_message: [
+                "18114495772705111902",
+                "2254271930739856077",
+                "2068851770",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+                "0",
+            ],
+        });
+        setRSAProvingTime(`${provingTime / 1000} s`);
+        setRSAProof(JSON.stringify(proof));
+        setRSAPublicSignals(JSON.stringify(publicSignals));
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 break-all">
+            <h1 className="text-4xl font-bold mb-8">
+                MoPro Website Prover Tests
+            </h1>
+            <div className="mb-8">
+                <h2 className="fix text-2xl font-bold mb-4">Multiplier2</h2>
+                <button className="btn mr-4" onClick={multiplier}>
+                    Prove
+                </button>
+                {/* <button className="btn">Verify</button> */}
+                <p className="mt-2">{multiplier2ProvingTime}</p>
+            </div>
+            <div className="bg-sky-200">
+                {multiplier2Proof && <h3>proof</h3>}
+                <p className="mt-2 break-all ">{multiplier2Proof}</p>
+                {multiplier2PublicSignals && <h3>public signals</h3>}
+                <p className="mt-2 break-all ">{multiplier2PublicSignals}</p>
+            </div>
+            <div className="mb-8">
+                <h2 className="text-2xl font-bold mb-4">Keccak256</h2>
+                <button className="btn mr-4" onClick={keccak256}>
+                    Prove
+                </button>
+                {/* <button className="btn">Verify</button> */}
+                <p className="mt-2">{keccak256ProvingTime}</p>
+            </div>
+            <div className="bg-sky-200 max-w-4xl">
+                {keccak256Proof && <h3>proof</h3>}
+                <p className="mt-2 break-all">{keccak256Proof}</p>
+                {keccak256PublicSignals && <h3>public signals</h3>}
+                <p className="mt-2 break-all">{keccak256PublicSignals}</p>
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold mb-4">RSA</h2>
+                <button className="btn mr-4" onClick={RSA}>
+                    Prove
+                </button>
+                {/* <button className="btn">Verify</button> */}
+                <p className="mt-2">{RSAProvingTime}</p>
+            </div>
+            <div className="bg-sky-200">
+                {RSAProof && <h3>proof</h3>}
+                <p className="mt-2 break-all">{RSAProof}</p>
+                {RSAPublicSignals && <h3>public signals</h3>}
+                <p className="mt-2 break-all">{RSAPublicSignals}</p>
+            </div>
+        </main>
+    );
 }
